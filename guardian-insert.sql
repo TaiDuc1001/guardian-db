6b5152130d4b531d989a260590c5067f25e50062
TXTuanAnh1668 <tuananhvn0203@gmail.com>
2024-10-18 12:34:35 +0700

    Chores:delete category 7

--- guardian-insert.sql
+++ guardian-insert.sql
@@ -27,7 +27,6 @@ INSERT INTO Category (CategoryID, CategoryName) VALUES
 ('CAT004', 'Books'),
 ('CAT005', 'Toys'),
 ('CAT006', 'Food'),
-('CAT007', 'Beauty');
 
 
 INSERT INTO Rank_ (RankID, RankName) VALUES 
