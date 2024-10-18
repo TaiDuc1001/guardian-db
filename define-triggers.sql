975ffeed8fc43e86a94e2499ebe5b1b5c6b205f2
taiduc1001 <phantaiduc2005@gmail.com>
2024-10-18 20:08:01 +0700

    Fix: indentation & new line

--- define-triggers.sql
+++ define-triggers.sql
@@ -1,4 +1,5 @@
 ﻿USE QL_Guardian
+
 GO
 
 CREATE TRIGGER UpdateProductInventory
@@ -7,13 +8,21 @@ AFTER INSERT
 AS
 BEGIN
 	UPDATE [dbo].[Product]
-	SET [StockQuantity]	= [StockQuantity] - (SELECT [Quantity] FROM inserted WHERE[dbo].[Product].ProductID = inserted.ProductID)
-	WHERE [dbo].[Product].ProductID IN (SELECT[ProductID] FROM inserted)	
+	SET [StockQuantity]	= [StockQuantity] - (
+      SELECT [Quantity] 
+      FROM inserted 
+      WHERE [dbo].[Product].ProductID = inserted.ProductID
+    )
+    WHERE [dbo].[Product].ProductID IN (
+      SELECT[ProductID] FROM inserted
+    )	
 END
-SELECT*FROM Product
-SELECT*FROM ProductOrder
+
+SELECT * FROM Product
+SELECT * FROM ProductOrder
 INSERT INTO ProductOrder (OrderID, ProductID, VoucherID, Quantity, DiscountedAmount, FinalPrice) VALUES 
 ('O002', 'P001', 'V001', 4, 10000, 1490000)
+
 GO
 CREATE TRIGGER CalculateDiscountedAmount
 ON [dbo].[ProductOrder]
@@ -21,30 +30,30 @@ AFTER INSERT, UPDATE
 AS
 BEGIN
 	UPDATE [dbo].[ProductOrder]
-	SET [DiscountedAmount] = (p.[Price] * po.[Quantity])*(v.[DiscountPercentage]/100.0)
-	FROM ProductOrder po 
-	JOIN Voucher v ON po.[VoucherID] = v.[VoucherID]
-	JOIN Product p ON po.[ProductID] = p.[ProductID]
-	WHERE po.[OrderID] IN (SELECT[OrderID] FROM inserted)
-
-
+	SET [DiscountedAmount] = (
+      p.[Price] * po.[Quantity]) * (v.[DiscountPercentage]/100.0)
+      FROM ProductOrder po 
+      JOIN Voucher v ON po.[VoucherID] = v.[VoucherID]
+      JOIN Product p ON po.[ProductID] = p.[ProductID]
+      WHERE po.[OrderID] IN (SELECT[OrderID] FROM inserted
+    )
 
 	UPDATE[dbo].[ProductOrder]
-	SET [FinalPrice] = (p.[Price] * po.[Quantity]) - po.[DiscountedAmount]
-	FROM ProductOrder po
-	JOIN Product p ON po.[ProductID] = p.[ProductID]
-	WHERE po.[OrderID] IN (SELECT[OrderID] FROM inserted)
-
+	SET [FinalPrice] = (
+      p.[Price] * po.[Quantity]) - po.[DiscountedAmount]
+      FROM ProductOrder po
+      JOIN Product p ON po.[ProductID] = p.[ProductID]
+      WHERE po.[OrderID] IN (SELECT[OrderID] FROM inserted
+    )
 END
+
 INSERT INTO ProductOrder(OrderID, ProductID, VoucherID, Quantity) VALUES ('O007','P004','V002', 4)
 SELECT*FROM ProductOrder
 
 SELECT*FROM Voucher
 
-
-
 GO
---Từ 10k VNĐ = 1Đ
+
 CREATE TRIGGER UpdatePoints
 ON [dbo].[Order_]
 AFTER INSERT, UPDATE
