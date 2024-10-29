f21f088cd4d53aee844ebf11563bd4ffde846563
taiduc1001 <phantaiduc2005@gmail.com>
2024-10-29 18:51:59 +0700

    Feat: sync with ert (old changes), add warehouses

--- define-tables.sql
+++ define-tables.sql
@@ -1,12 +1,10 @@
 ﻿CREATE DATABASE QL_Guardian
 USE QL_Guardian
 
-CREATE TABLE Image_
+CREATE TABLE Category
 (
-	ImageID VARCHAR(20) NOT NULL PRIMARY KEY,
-	ImagePath VARCHAR(100),
-	AltText TEXT,
-	BigVersionPath VARCHAR(100)
+	CategoryID VARCHAR(20) NOT NULL PRIMARY KEY,
+	CategoryName VARCHAR(50)
 )
 
 CREATE TABLE Brand
@@ -16,13 +14,7 @@ CREATE TABLE Brand
 	isTopBrand TINYINT
 )	
 
-CREATE TABLE Category
-(
-	CategoryID VARCHAR(20) NOT NULL PRIMARY KEY,
-	CategoryName VARCHAR(50)
-)
-
-CREATE TABLE Rank_
+CREATE TABLE Rank
 (
 	RankID varchar(20) NOT NULL PRIMARY KEY,
 	RankName varchar(15)
@@ -40,15 +32,60 @@ CREATE TABLE Product
 	Ingredients NVARCHAR(100),
 	DescriptionProduct TEXT,
 	Uses TEXT,
+	Ingredients TEXT,
+	Unit VARCHAR(10),
 	InstructionManualDescription TEXT, 
 	InstructionStoreDescription TEXT,
-	StockQuantity INT,
 	SoldCount INT,
 	CONSTRAINT FK_CategoryID FOREIGN KEY (CategoryID) REFERENCES Category(CategoryID),
 	CONSTRAINT FK_BrandID FOREIGN KEY (BrandID) REFERENCES Brand(BrandID)
 )
 
-CREATE TABLE Address_
+CREATE TABLE Image
+(
+	ImageID VARCHAR(20) NOT NULL PRIMARY KEY,
+	ImagePath VARCHAR(100),
+	AltText TEXT,
+	BigVersionPath VARCHAR(100),
+	ProductID VARCHAR(20),
+	OrdinalNumber INT,
+	CONSTRAINT FK_ProductID2 FOREIGN KEY (ProductID) REFERENCES Product(ProductID)
+)
+
+CREATE TABLE User_
+(
+	UserID VARCHAR(20) NOT NULL PRIMARY KEY,
+	FirstName NVARCHAR(10) NOT NULL,
+	MiddleName NVARCHAR(10) NOT NULL,
+	LastName NVARCHAR(10) NOT NULL,
+	PhoneNumber VARCHAR(11) NOT NULL UNIQUE,
+	Email VARCHAR(50) NOT NULL UNIQUE,
+	UserPassword VARCHAR(30) NOT NULL,
+	Point INT,
+	RankID VARCHAR(20),
+	TotalOrder INT DEFAULT 0,
+	Birthdate DATE,	
+	Sex CHAR(3),
+	CONSTRAINT FK_RankID FOREIGN KEY (RankID) REFERENCES Rank(RankID)
+)
+ALTER TABLE User_
+ADD FullName AS CONCAT(
+	COALESCE(LastName, ''),
+	' ',
+	COALESCE(MiddleName, ''),
+	' ',
+	COALESCE(FirstName, '')
+)
+
+CREATE TABLE Event
+(
+	EventID VARCHAR(20) NOT NULL PRIMARY KEY,
+	EventName NVARCHAR(50) NOT NULL,
+	StartDate DATE DEFAULT GETDATE(),
+	EndDate DATE
+)
+
+CREATE TABLE Address
 (
 	AddressID VARCHAR(20) NOT NULL PRIMARY KEY,
 	PhoneNumber VARCHAR(11) NOT NULL UNIQUE,
@@ -57,37 +94,138 @@ CREATE TABLE Address_
 	Ward NVARCHAR(30) NOT NULL,
 	District NVARCHAR(20) NOT NULL,
 	City NVARCHAR (30) NOT NULL,
-	Country NVARCHAR(20) NOT NULL
+	Country NVARCHAR(20) NOT NULL,
+	UserID VARCHAR(20),
+	isDefault TINYINT DEFAULT 0,
+	Type NVARCHAR(20) DEFAULT N'Nhà riêng',
+	CONSTRAINT FK_UserID2 FOREIGN KEY (UserID) REFERENCES User_(UserID)
 )
 
-ALTER TABLE Address_
+ALTER TABLE Address
 ADD FullAddress AS CONCAT(HouseNumber, ' ', Street, ', ', Ward, ', ', District, ', ', City, ', ', Country)
 
 
-CREATE TABLE User_
+CREATE TABLE OrderStatus
 (
-	UserID VARCHAR(20) NOT NULL PRIMARY KEY,
+	StatusID VARCHAR(20) NOT NULL PRIMARY KEY,
+	StatusName NVARCHAR(50) 
+	CHECK (StatusName IN (N'Đã đặt hàng', N'Đã xác nhận', N'Đang giao hàng', N'Đã giao hàng', N'Đã hủy'))
+)
+
+
+CREATE TABLE Voucher
+(
+	VoucherID VARCHAR(20) NOT NULL PRIMARY KEY,
+	VoucherCode VARCHAR(20) NOT NULL,
+	Name VARCHAR(50),
+	MinimumPrice INT DEFAULT 0,
+	DiscountPrice INT,
+	DiscountPercentage DECIMAL(2,0),
+	VoucherDescription TEXT,
+	EventID VARCHAR(20),
+	CONSTRAINT FK_EventID1 FOREIGN KEY (EventID) REFERENCES Event(EventID)
+)
+
+CREATE TABLE Review
+(
+	ReviewID VARCHAR(20) NOT NULL PRIMARY KEY,
+	ReviewTime DATE,
+	ReviewMessage TEXT,
+	Stars DECIMAL(3,1)
+)
+
+
+CREATE TABLE Shipper
+(
+	ShipperID VARCHAR(20) NOT NULL PRIMARY KEY,
+	Salary MONEY,
 	FirstName NVARCHAR(10),
 	MiddleName NVARCHAR(10),
 	LastName NVARCHAR(10),
 	PhoneNumber VARCHAR(11) NOT NULL UNIQUE,
 	Email VARCHAR(50) NOT NULL UNIQUE,
-	UserPassword VARCHAR(30) NOT NULL,
-	Point INT,
-	RankID varchar(20),
-	TotalOrder INT,
-	Birthdate DATE,	
-	Sex CHAR(3),
-	CONSTRAINT FK_RankID FOREIGN KEY (RankID) REFERENCES Rank_(RankID)
+	Password VARCHAR(30) NOT NULL,
+	TotalOrder INT
 )
-ALTER TABLE User_
-ADD FullName AS CONCAT(LastName, ' ', MiddleName, ' ', FirstName);
+ALTER TABLE Shipper
+ADD FullName AS CONCAT(
+	COALESCE(LastName, ''),
+	' ',
+	COALESCE(MiddleName, ''),
+	' ',
+	COALESCE(FirstName, '')
+)
 
-CREATE TABLE UserBagProducts --giỏ hàng
+
+CREATE TABLE Order_
+(
+	OrderID VARCHAR(20) NOT NULL PRIMARY KEY,
+	UserID VARCHAR(20),
+	TotalPrice DECIMAL(20,0),
+	DateOrdered DATE,
+	DeliveryAddressID VARCHAR(20),
+	CONSTRAINT FK_UserID3 FOREIGN KEY (UserID) REFERENCES User_(UserID),
+	CONSTRAINT FK_AddressID FOREIGN KEY (DeliveryAddressID) REFERENCES Address(AddressID)
+)
+
+
+CREATE TABLE CentralWarehouse
+(
+	CentralWID VARCHAR(20) NOT NULL PRIMARY KEY,
+	CentralWName NVARCHAR(50),
+	Region NVARCHAR(50),
+	AddressID VARCHAR(20),
+	CONSTRAINT FK_AddressID1 FOREIGN KEY (AddressID) REFERENCES Address(AddressID)
+)
+
+CREATE TABLE BranchWarehouse
+(
+	BranchWID VARCHAR(20) NOT NULL PRIMARY KEY,
+	BranchWName NVARCHAR(50),
+	Region NVARCHAR(50),
+	AddressID VARCHAR(20),
+	CentralWID VARCHAR(20),
+	CONSTRAINT FK_AddressID2 FOREIGN KEY (AddressID) REFERENCES Address(AddressID),
+	CONSTRAINT FK_CentralWID FOREIGN KEY (CentralWID) REFERENCES CentralWarehouse(CentralWID)
+)
+
+CREATE TABLE CentralW_Product
+(
+	ProductID VARCHAR(20),
+	CentralWID VARCHAR(20),
+	StockQuantity INT DEFAULT 0,
+	CONSTRAINT FK_ProductID_CentralWID PRIMARY KEY (ProductID, CentralWID),
+	CONSTRAINT FK_ProductID1 FOREIGN KEY (ProductID) REFERENCES Product(ProductID),
+	CONSTRAINT FK_CentralWID1 FOREIGN KEY (CentralWID) REFERENCES CentralWarehouse(CentralWID)
+)
+
+CREATE TABLE BranchW_Product
+(
+	ProductID VARCHAR(20),
+	BranchWID VARCHAR(20),
+	StockQuantity INT DEFAULT 0,
+	CONSTRAINT FK_ProductID_BranchWID PRIMARY KEY (ProductID, BranchWID),
+	CONSTRAINT FK_ProductID3 FOREIGN KEY (ProductID) REFERENCES Product(ProductID),
+	CONSTRAINT FK_BranchWID FOREIGN KEY (BranchWID) REFERENCES BranchWarehouse(BranchWID)
+)
+
+CREATE TABLE Gift
+(
+	MainProductID VARCHAR(20),
+	GiftProductID VARCHAR(20),
+	Quantity INT DEFAULT 1,
+	StartDate DATE,
+	EndDate DATE,
+	CONSTRAINT FK_MainProductID_GiftProductID PRIMARY KEY (MainProductID, GiftProductID),
+	CONSTRAINT FK_MainProductID FOREIGN KEY (MainProductID) REFERENCES Product(ProductID),
+	CONSTRAINT FK_GiftProductID FOREIGN KEY (GiftProductID) REFERENCES Product(ProductID)
+)
+
+CREATE TABLE Cart --giỏ hàng
 (
 	ProductID VARCHAR(20),
 	UserID VARCHAR(20),
-	Quantity SMALLINT,
+	Quantity SMALLINT DEFAULT 1,
 	CONSTRAINT FK_ProductID_UserID PRIMARY KEY (ProductID, UserID),
 	CONSTRAINT FK_ProductID FOREIGN KEY (ProductID) REFERENCES Product(ProductID),
 	Constraint FK_UserID FOREIGN KEY(UserID) REFERENCES User_(UserID)
@@ -102,40 +240,18 @@ CREATE TABLE LikedProducts -- sản phẩm đã thích
 	Constraint FK_UserID1 FOREIGN KEY(UserID) REFERENCES User_(UserID)
 )
 
-CREATE TABLE Voucher
-(
-	VoucherID VARCHAR(20) NOT NULL PRIMARY KEY,
-	Code VARCHAR(20) NOT NULL UNIQUE,
-	Name VARCHAR(50),
-	MinimumPrice INT,
-	DiscountPRice INT,
-	DiscountPercentage DECIMAL(2,0),
-	VoucherDescription TEXT
-)
 
-CREATE TABLE Order_
+CREATE TABLE ReviewOf
 (
-	OrderID VARCHAR(20) NOT NULL PRIMARY KEY,
-	UserID VARCHAR(20),
-	TotalPrice DECIMAL(20,0),
-	DateOrdered DATE,
-	DeliveryAddressID VARCHAR(20),
-	CONSTRAINT FK_UserID3 FOREIGN KEY (UserID) REFERENCES User_(UserID),
-	CONSTRAINT FK_AddressID FOREIGN KEY (DeliveryAddressID) REFERENCES Address_(AddressID)
-)
-
-CREATE TABLE Review
-(
-	ReviewID VARCHAR(20) NOT NULL PRIMARY KEY,
+	ReviewID VARCHAR(20),
 	OrderID VARCHAR(20),
 	ProductID VARCHAR(20),
 	UserID VARCHAR(20),
-	ReviewTime DATE,
-	ReviewContent TEXT,
-	Stars DECIMAL(3,1)
-	CONSTRAINT FK_OrderID3 FOREIGN KEY (OrderID) REFERENCES Order_(OrderID),
-	CONSTRAINT FK_UserID5 FOREIGN KEY (UserID) REFERENCES User_(UserID),
-	CONSTRAINT FK_ProductID7 FOREIGN KEY (ProductID) REFERENCES Product(ProductID)
+	CONSTRAINT FK_ReviewID_OrderID_ProductID_UserID PRIMARY KEY (ReviewID, OrderID, ProductID, UserID),
+	CONSTRAINT FK_ReviewID FOREIGN KEY (ReviewID) REFERENCES Review(ReviewID),
+	CONSTRAINT FK_OrderID1 FOREIGN KEY (OrderID) REFERENCES Order_(OrderID),
+	CONSTRAINT FK_ProductID1 FOREIGN KEY (ProductID) REFERENCES Product(ProductID),
+	CONSTRAINT FK_UserID3 FOREIGN KEY (UserID) REFERENCES User_(UserID)
 )
 
 CREATE TABLE UserVoucher
@@ -147,34 +263,18 @@ CREATE TABLE UserVoucher
 	CONSTRAINT FK_VoucherID FOREIGN KEY (VoucherID) REFERENCES Voucher(VoucherID),
 	Constraint FK_UserID2 FOREIGN KEY(UserID) REFERENCES User_(UserID)
 )
-CREATE TABLE OrderStatus
-(
-	StatusID VARCHAR(20),
-	StatusName NVARCHAR(15)
-)
 
-CREATE TABLE Shipper
-(
-	ID VARCHAR(20) NOT NULL PRIMARY KEY,
-	Salary MONEY,
-	FirstName NVARCHAR(10),
-	MiddleName NVARCHAR(10),
-	LastName NVARCHAR(10),
-	PhoneNumber VARCHAR(11) NOT NULL UNIQUE,
-	Email VARCHAR(50) NOT NULL UNIQUE,
-	Password VARCHAR(30) NOT NULL,
-	TotalOrder INT
-)
+
 
 CREATE TABLE ShipperOrder
 (
 	OrderID VARCHAR(20),
 	ShipperID VARCHAR(20),
 	StatusID VARCHAR(20),
-	Time DATETIME,
-	CONSTRAINT FK_OrderID2_ShipperID PRIMARY KEY (OrderID, ShipperID, StatusID, Time),
+	Milestone DATETIME,
+	CONSTRAINT FK_OrderID2_ShipperID PRIMARY KEY (OrderID, ShipperID, StatusID, Milestone),
 	CONSTRAINT FK_OrderID2 FOREIGN KEY (OrderID) REFERENCES Order_(OrderID),
-	CONSTRAINT FK_ShipperID FOREIGN KEY (ShipperID) REFERENCES Shipper(ID)
+	CONSTRAINT FK_ShipperID FOREIGN KEY (ShipperID) REFERENCES Shipper(ID),
 	CONSTRAINT FK_StatusID FOREIGN KEY (StatusID) REFERENCES OrderStatus(StatusID)
 )
 
@@ -183,8 +283,8 @@ CREATE TABLE ProductOrder
 	OrderID VARCHAR(20),
 	ProductID VARCHAR(20),
 	VoucherID VARCHAR(20),
-	Quantity SMALLINT,
-	DiscountedAmount INT,
+	Quantity SMALLINT DEFAULT 1,
+	DiscountedAmount INT DEFAULT 0,
 	FinalPrice INT,
 	CONSTRAINT FK_OrderID_ProductID2_VoucherID1 PRIMARY KEY (OrderID, ProductID, VoucherID),
 	CONSTRAINT FK_OrderID FOREIGN KEY (OrderID) REFERENCES Order_(OrderID),
@@ -192,25 +292,17 @@ CREATE TABLE ProductOrder
 	CONSTRAINT FK_VoucherID1 FOREIGN KEY (VoucherID) REFERENCES Voucher(VoucherID)
 )
 
-CREATE TABLE Event_
-(
-	EventID VARCHAR(20) NOT NULL PRIMARY KEY,
-	EventName NVARCHAR(50),
-	StartDate DATE,
-	EndDaye DATE
-)
-
-CREATE TABLE EventProduct
+CREATE TABLE EventProducts
 (
 	EventID VARCHAR(20),
 	ProductID VARCHAR(20),
-	DiscountPercent TINYINT,
+	DiscountPercent DECIMAL(2,0),
 	CONSTRAINT FK_EventID_ProductID3 PRIMARY KEY(EventID, ProductID),
-	CONSTRAINT FK_EventID FOREIGN KEY (EventID) REFERENCES Event_(EventID),
+	CONSTRAINT FK_EventID FOREIGN KEY (EventID) REFERENCES Event(EventID),
 	CONSTRAINT FK_ProductID3 FOREIGN KEY (ProductID) REFERENCES Product(ProductID)
 )
 
-CREATE TABLE VoucherProduct
+CREATE TABLE VoucherProducts
 (
 	ProductID VARCHAR(20),
 	VoucherID VARCHAR(20),
@@ -223,14 +315,20 @@ CREATE TABLE PaymentTerm
 (
 	PaymentTermID VARCHAR(20) NOT NULL PRIMARY KEY,
 	PaymentTermName NVARCHAR(20)
+	CHECK (PaymentTermName IN (
+		N'Thanh toán khi nhận hàng', 
+		N'Thanh toán qua thẻ ngân hàng', 
+		N'Thanh toán qua ví điện tử'
+		)
+	),
 )
 CREATE TABLE Invoice
 (
 	InvoiceKey VARCHAR(20) NOT NULL PRIMARY KEY,
-	Serial NVARCHAR(50),
+	Serial VARCHAR(50) NOT NULL,
 	InvoiceNumber INT,
-	InvoiceDate DATE,
-	MCQT VARCHAR(20) NOT NULL UNIQUE,
+	InvoiceDate DATE DEFAULT GETDATE(),
+	MCQT VARCHAR(20) NOT NULL,
 	BrandID VARCHAR(20),
 	UserID VARCHAR(20),
 	PaymentTermID VARCHAR(20),
@@ -240,20 +338,20 @@ CREATE TABLE Invoice
 	CONSTRAINT FK_PaymentTermID FOREIGN KEY (PaymentTermID) REFERENCES PaymentTerm (PaymentTermID)
 )
 
-CREATE TABLE Cart
-(
-	ProductID VARCHAR(20),
-	UserID VARCHAR(20),
-	Quantity INT,
-	CONSTRAINT FK_ProductID6_UserID6 PRIMARY KEY (ProductID, UserID),
-	CONSTRAINT FK_ProductID6 FOREIGN KEY (ProductID) REFERENCES Product(ProductID),
-	CONSTRAINT FK_UserID6 FOREIGN KEY (UserID) REFERENCES User_(UserID)
-)
 
 CREATE TABLE Genre
 (
 	ID VARCHAR(20) NOT NULL PRIMARY KEY,
-	GenreName NVARCHAR(50) NOT NULL
+	GenreName NVARCHAR(50) NOT NULL CHECK (
+		GenreName IN (
+			N'Chính trị',
+			N'Kinh tế',
+			N'Xã hội',
+			N'Văn hóa',
+			N'Giáo dục',
+			N'Khoa học'
+		)
+	)
 )
 
 CREATE TABLE News
@@ -264,5 +362,5 @@ CREATE TABLE News
 	HTMLPath VARCHAR(100),
 	ThumbnailID VARCHAR(20),
 	CONSTRAINT FK_GenreID FOREIGN KEY (GenreID) REFERENCES Genre(ID),
-	CONSTRAINT FK_ThumbnailID FOREIGN KEY (ThumbnailID) REFERENCES Image_(ImageID)
+	CONSTRAINT FK_ThumbnailID FOREIGN KEY (ThumbnailID) REFERENCES Image(ImageID)
 )
